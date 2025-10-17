e308b839456003235c887bfc7e6c5473f9d9470d
prajapatishivam00336-gif <prajapatishivam00336@gmail.com>
2025-10-17 14:02:33 +0530

    Experiment 1a.c

---
+++ Experiment 1a.c
@@ -0,0 +1,17 @@
+name:shivam prajapati
+class:civil
+divisin:B
+UIN:251C005
+*************************
+#include <stdio.h>
+
+int main()
+{
+    printf("name is shivam prajapati\n");
+    printf("age is 18\n");
+    printf("class is civil\n");
+    printf("divisin B\n");
+    printf("UIN is 251C005\n");
+    
+    return 0;
+}
