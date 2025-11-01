09b5cda53e73873ccf3c00bd2050a5370b7120e3
prajapatishivam00336-gif <prajapatishivam00336@gmail.com>
2025-11-01 18:16:47 +0530

    Experiment5.c

---
+++ Experiment5.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+int factorial_iterative(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+    fact *= i;
+    }
+    return fact;
+}
+int factorial_recursive(int n) {
+    if (n == 0 || n == 1)
+    return 1;
+    else
+    return n * factorial_recursive(n - 1);
+}
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+printf("\nFactorial using iteration: %d", factorial_iterative(num));
+printf("\nFactorial using recursion: %d", factorial_recursive(num));
+
+ return 0;
+}
